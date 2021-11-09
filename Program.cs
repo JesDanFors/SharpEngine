1b828882ef30887ea55a2f6b77f5ff47f6fd5735
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-11-09 11:02:46 +0100

    Add the OpenGL framework

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GLFW;
+using static OpenGL.Gl;
 
 namespace SharpEngine
 {
@@ -10,6 +11,15 @@ namespace SharpEngine
             Glfw.Init();
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
             Glfw.MakeContextCurrent(window);
+
+            float[] vertices = new float[]
+            {
+                -.5f, -.5f, 0f,
+                .5f, -.5f, 0f,
+                0f, .5f, 0f
+            };
+
+            var vertexArray = glGenVertexArray();
 
             while (!Glfw.WindowShouldClose(window))
             {
