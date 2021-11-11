5c17a2515d42a847ba806d915752559b34ab6564
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-11-11 13:08:28 +0100

    Add the vector struct in its own file

--- Program.cs
+++ Program.cs
@@ -1,67 +1,15 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using GLFW;
 using static OpenGL.Gl;
 
 namespace SharpEngine
 {
-    struct Vector
-    {
-        public float x, y, z;
-
-        public Vector(float x, float y, float z)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        public Vector(float x, float y)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = 0;
-        }
-
-        public static Vector operator *(Vector v, float f)
-        {
-            return new Vector(v.x * f, v.y * f, v.z * f);
-        }
-        public static Vector operator /(Vector v, float f)
-        {
-            return new Vector(v.x / f, v.y / f, v.z / f);
-        }
-
-        public static Vector operator +(Vector v, Vector u)
-        {
-            return new Vector(v.x + u.x, v.y + u.y, v.z + u.z);
-        }public static Vector operator -(Vector v, Vector u)
-        {
-            return new Vector(v.x - u.x, v.y - u.y, v.z - u.z);
-        }
-        public static Vector Max(Vector a, Vector b)
-        {
-            Vector c = new Vector();
-            c.x = MathF.Max(a.x, b.x);
-            c.y = MathF.Max(a.y, b.y);
-            return c;
-        }
-        public static Vector Min(Vector a, Vector b)
-        {
-            Vector c = new Vector(0,0)
-            {
-                x = MathF.Min(a.x, b.x),
-                y = MathF.Min(a.y, b.y)
-            };
-            return c;
-        }
-    }
     class Program
     {
         static Vector[] vertices = new Vector[]{
-            new Vector(-.1f, -.1f),
-            new Vector(.1f, -.1f),
-            new Vector(0f, .1f),
+            new Vector(-.4f, -.4f),
+            new Vector(.4f, -.4f),
+            new Vector(.0f, .6f),
             //new Vector(.4f, .4f),
             //new Vector(.6f, .4f),
             //new Vector(.5f, .6f)
---
+++ Vector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SharpEngine
+{
+    struct Vector
+    {
+        public float x, y, z;
+
+        public Vector(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = 0;
+        }
+
+        public static Vector operator *(Vector v, float f)
+        {
+            return new Vector(v.x * f, v.y * f, v.z * f);
+        }
+        public static Vector operator /(Vector v, float f)
+        {
+            return new Vector(v.x / f, v.y / f, v.z / f);
+        }
+
+        public static Vector operator +(Vector v, Vector u)
+        {
+            return new Vector(v.x + u.x, v.y + u.y, v.z + u.z);
+        }public static Vector operator -(Vector v, Vector u)
+        {
+            return new Vector(v.x - u.x, v.y - u.y, v.z - u.z);
+        }
+        public static Vector Max(Vector a, Vector b)
+        {
+            Vector c = new Vector();
+            c.x = MathF.Max(a.x, b.x);
+            c.y = MathF.Max(a.y, b.y);
+            return c;
+        }
+        public static Vector Min(Vector a, Vector b)
+        {
+            Vector c = new Vector(0,0)
+            {
+                x = MathF.Min(a.x, b.x),
+                y = MathF.Min(a.y, b.y)
+            };
+            return c;
+        }
+    }
+}
